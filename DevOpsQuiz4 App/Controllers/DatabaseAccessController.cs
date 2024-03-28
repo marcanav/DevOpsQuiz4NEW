4b03d1bc9b105f22bb344869dc1b0dd9d72eada3
Maria Nav <mnavar17@my.centennialcollege.ca>
2024-03-28 14:48:46 -0400

    2nd commit

---
+++ DevOpsQuiz4 App/Controllers/DatabaseAccessController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DevOpsQuiz4_App.Controllers
+{
+    public class DatabaseAccessController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
