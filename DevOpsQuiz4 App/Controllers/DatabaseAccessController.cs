68a4ffa52083db6064b38f806fa3e5703127517f
Maria Navarrete <mnavar17@my.centennialcollege.ca>
2024-03-28 15:33:44 -0400

    Update DatabaseAccessController.cs

--- DevOpsQuiz4 App/Controllers/DatabaseAccessController.cs
+++ DevOpsQuiz4 App/Controllers/DatabaseAccessController.cs
@@ -6,7 +6,7 @@ namespace DevOpsQuiz4_App.Controllers
     {
         public string GetDatabaseAccessMessage()
         {
-            return "Database access has already been done on April 14, 2023 by John Doe"; // Replace "John Doe" with your name
+            return "Database access has already been done on April 14, 2023 by Maria Navarrete";
         }
     }
 }
